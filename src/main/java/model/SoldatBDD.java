b9b415bb3fba503ccc1ffaccc2c859ab06d37eab
sara-molie <125445622+sara-molie@users.noreply.github.com>
2025-01-04 02:13:17 +0100

    Add files via upload
    soldat

--- src/main/java/model/SoldatBDD.java
+++ src/main/java/model/SoldatBDD.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoldatBDD {
 
@@ -21,13 +23,15 @@ public class SoldatBDD {
     }
 
     // Ajouter un soldat
-    public int ajouterSoldatEtRecupererId(String login, int pointDeVie) {
-        String sql = "INSERT INTO soldat (login_user, point_de_vie) VALUES (?, ?)";
+    public int ajouterSoldatEtRecupererId(String login, int pointDeVie, int x, int y) {
+        String sql = "INSERT INTO soldat (login_user, point_de_vie, x_position , y_position) VALUES (?, ?, ?, ?)";
         try (Connection cnx = initConnection();
              PreparedStatement stmt = cnx.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
             stmt.setString(1, login);
             stmt.setInt(2, pointDeVie);
+            stmt.setInt(3, x);
+            stmt.setInt(4, y);
 
             int rowsInserted = stmt.executeUpdate();
             if (rowsInserted > 0) {
@@ -42,6 +46,21 @@ public class SoldatBDD {
         return -1; // Retourne -1 en cas d'échec
     }
 
+    public boolean existeSoldatPosition(int x, int y) {
+        String sql = "SELECT 1 FROM soldat WHERE x_position = ? AND y_position = ?";
+        try (Connection cnx = initConnection();
+             PreparedStatement stmt = cnx.prepareStatement(sql)) {
+
+            stmt.setInt(1, x);
+            stmt.setInt(2, y);
+
+            ResultSet rs = stmt.executeQuery();
+            return rs.next(); // Retourne vrai si une ligne est trouvée
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false; // En cas d'erreur, considérer qu'il n'y a pas de soldat
+    }
 
     // Obtenir tous les soldats d'un joueur
     public ResultSet getSoldatsByJoueur(int idJoueur) {
@@ -93,19 +112,32 @@ public class SoldatBDD {
         }
     }
     
-    public int compterSoldatsPossedesParUtilisateur(String userLogin) throws SQLException {
-        String sql = "SELECT COUNT(*) FROM soldat WHERE login_user = ?";
-        try (Connection conn = initConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, userLogin);
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-                return 0;  // Retourne 0 si aucun résultat n'est trouvé
+    public List<Soldat> getSoldatsByUser(String loginUser) {
+        String sql = "SELECT id_soldat, x_position, y_position, point_de_vie FROM soldat WHERE login_user = ?";
+        List<Soldat> soldats = new ArrayList<>();
+        try (Connection cnx = initConnection();
+             PreparedStatement stmt = cnx.prepareStatement(sql)) {
+
+            stmt.setString(1, loginUser);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id_soldat");
+                int x = rs.getInt("x_position");
+                int y = rs.getInt("y_position");
+                int pointsDeVie = rs.getInt("point_de_vie");
+                Soldat soldat = new Soldat(x, y, loginUser, pointsDeVie);
+                soldat.setId(id);
+                soldats.add(soldat);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return soldats;
     }
+
+    
+  
+    
 
     
     
